4667076833ec89ea0db6bd8a75838a2eb50a9961
Prateek Surana <prateeksurana3255@gmail.com>
2018-08-15 17:35:59 +0530

    bear_and_steady medium problem added

---
+++ bear_and_steady_gene.py
@@ -0,0 +1,38 @@
+
+def contains(gene,remove):
+    for i in remove:
+        if gene.count(i)<remove[i]:
+            return False
+    return True
+def steadyGene(gene):
+    genes={'C':0,'G':0,'A':0,'T':0}
+    remove={'C':0,'G':0,'A':0,'T':0}
+    for i in genes:
+        genes[i]=gene.count(i)
+        if genes[i]>len(gene)//4:
+            remove[i]=genes[i]-len(gene)//4
+    mi=len(gene)
+    start=0
+    print(genes,remove)
+    for i in range(len(gene)):
+        if contains(gene[start:i],remove):
+            while contains(gene[start:i],remove):
+                start+=1
+            start-=1
+            if mi>(i-start):
+                mi=i-start
+                print(gene[start:i])
+            start+=1
+    return mi
+
+
+
+
+if __name__ == '__main__':
+
+    n = int(input())
+
+    gene = input()
+
+    result = steadyGene(gene)
+    print(result)
